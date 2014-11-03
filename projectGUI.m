5b0398d61fe51552db979fe631a6f1434056544d
rjgerth <rjgerth.95yj@gmail.com>
2014-11-03 15:28:39 -0500

    correct cell to string syntax

--- projectGUI.m
+++ projectGUI.m
@@ -89,8 +89,9 @@ function edit1_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of edit1 as text
 %        str2double(get(hObject,'String')) returns contents of edit1 as a double
 
-str1=get(hObject,'String');
-ansNum=str2num(str1);
+str1=get(hObject,'String')
+str2=str1{1}
+ansNum=str2num(str2);
 ansStr=sprintf('Your answer is %f. That is correct!', ansNum);
 
 set(handles.text1,'String',ansStr);
