c9302b0025e1b5948e6e8f6c55f4ccc3cb67e531
rjgerth <rjgerth.95yj@gmail.com>
2014-11-24 11:04:23 -0500

    Revert "grid formatting"
    This reverts commit b7a7111a227f6f78356cb58787f7554049d43eee.

--- gridandneuronconstruction.m
+++ gridandneuronconstruction.m
@@ -5,29 +5,22 @@ clc
 %easy
 easyNeuron=ones(5,7);
 easyNeuron([7,9,13,18,23,28])=.5; easyNeuron(8)=0;
-imagesc(easyNeuron); colormap gray; pbaspect([7 5 1]);
-set(gca,'xtick',linspace(0.5,7.5,8),'ytick',linspace(0.5,5.5,6));
-set(gca,'xgrid','on','ygrid','on','gridlinestyle','-');
+imagesc(easyNeuron); colormap gray
 
 %medium
 medNeuron=ones(5,7);
 medNeuron([8,12,18,23,24,28])=.5; medNeuron(13)=0;
-figure; imagesc(medNeuron); colormap gray; pbaspect([7 5 1]);
-set(gca,'xtick',linspace(0.5,7.5,8),'ytick',linspace(0.5,5.5,6));
-set(gca,'xgrid','on','ygrid','on','gridlinestyle','-');
+figure; imagesc(medNeuron); colormap gray
 
 %hard
 hardNeuron=ones(5,7);
 hardNeuron([7,8,14,18,22,23,27])=.5; hardNeuron(13)=0;
-figure; imagesc(hardNeuron); colormap gray; pbaspect([7 5 1]);
-set(gca,'xtick',linspace(0.5,7.5,8),'ytick',linspace(0.5,5.5,6));
-set(gca,'xgrid','on','ygrid','on','gridlinestyle','-');
+figure; imagesc(hardNeuron); colormap gray
 
 %% Grid/neuron overlay construction
 
-%easy 7x7 grid
-nx=7; ny=7;
-easyGrid=ones(nx,ny);
+%easy 5x7 grid
+easyGrid=ones(5,7);
 eShapeN = easyNeuron(2:4,2:6);
 sizeN=size(eShapeN);
 sizeG=size(easyGrid);
@@ -39,31 +32,18 @@ horzShift=randi([0,posHorz],1);
 
 easyGrid((vertShift+[1:sizeN(1)]),(horzShift+[1:sizeN(2)]))=eShapeN;
 
-nonAH=find(easyGrid == 1);
+nonAH=find(easyGrid > 0);
 ehints=nonAH(randi([1,length(nonAH)],1,4));
 
-easyGridDisp=zeros(nx,ny);
+easyGridDisp=zeros(5,7);
 easyGridDisp(ehints)=easyGrid(ehints);
 
-eXLabel=cell(1,(nx+1));
-for i=1:(nx+1)
-    eXLabel{i}=num2str(i-1);
-end
-
-eYLabel=cell(1,(ny+1));
-for i=1:(ny+1)
-    eYLabel{i}=num2str(i-1);
-end
-
 figure; imagesc(easyGridDisp)
-pbaspect([nx ny 1]);
-set(gca,'xtick',linspace(0.5,nx+0.5,nx+1),'ytick',linspace(0.5,ny+0.5,ny+1));
-set(gca,'XTickLabel',eXLabel,'YTickLabel',eYLabel);
-set(gca,'xgrid','on','ygrid','on','gridlinestyle','-');
 
-%med 13x13 grid
-nx=13; ny=13;
-medGrid=ones(nx,ny);
+
+%med 9x13 grid
+
+medGrid=ones(9,13);
 mShapeN = medNeuron(2:4,2:6);
 sizeN=size(mShapeN);
 sizeG=size(medGrid);
@@ -78,28 +58,14 @@ medGrid((vertShift+[1:sizeN(1)]),(horzShift+[1:sizeN(2)]))=mShapeN;
 nonNeuron=find(medGrid == 1);
 mhints=nonNeuron(randi([1,length(nonAH)],1,4));
 
-medGridDisp=zeros(nx,ny);
+medGridDisp=zeros(9,13);
 medGridDisp(mhints)=medGrid(mhints);
 
-mXLabel=cell(1,(nx+1));
-for i=1:(nx+1)
-    mXLabel{i}=num2str(i-1);
-end
-
-mYLabel=cell(1,(ny+1));
-for i=1:(ny+1)
-    mYLabel{i}=num2str(i-1);
-end
-
 figure; imagesc(medGridDisp)
-pbaspect([nx ny 1]);
-set(gca,'xtick',linspace(0.5,nx+0.5,nx+1),'ytick',linspace(0.5,ny+0.5,ny+1));
-set(gca,'XTickLabel',mXLabel,'YTickLabel',mYLabel);
-set(gca,'xgrid','on','ygrid','on','gridlinestyle','-');
 
 %hard 17x17 grid
-nx=17; ny=17;
-hardGrid=ones(nx,ny);
+
+hardGrid=ones(17);
 hShapeN = hardNeuron(2:4,2:6);
 sizeN=size(hShapeN);
 sizeG=size(hardGrid);
@@ -114,21 +80,7 @@ hardGrid((hvertShift+[1:sizeN(1)]),(hhorzShift+[1:sizeN(2)]))=hShapeN;
 nonNeuron=find(hardGrid == 1);
 hhints=nonNeuron(randi([1,length(nonNeuron)],1,2));
 
-hardGridDisp=zeros(nx,ny);
+hardGridDisp=zeros(17);
 hardGridDisp(hhints)=hardGrid(hhints);
 
-hXLabel=cell(1,(nx+1));
-for i=1:(nx+1)
-    hXLabel{i}=num2str(i-1);
-end
-
-hYLabel=cell(1,(ny+1));
-for i=1:(ny+1)
-    hYLabel{i}=num2str(i-1);
-end
-
 figure; imagesc(hardGridDisp)
-pbaspect([nx ny 1]);
-set(gca,'xtick',linspace(0.5,nx+0.5,nx+1),'ytick',linspace(0.5,ny+0.5,ny+1));
-set(gca,'XTickLabel',hXLabel,'YTickLabel',hYLabel);
-set(gca,'xgrid','on','ygrid','on','gridlinestyle','-');
